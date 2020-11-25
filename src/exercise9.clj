8adaafaac18d2f9b3e287d66d58840e70d28eefd
julianquino <julian.quino2@gmail.com>
2020-11-24 21:19:13 -0300

    NEW: exercise 9 complete, with the tests passing

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,11 +1,10 @@
-(ns exercise9)
+(ns exercise9
+	(:require [clojure.string :as string]))
 
 (defn word-count 
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (throw (Exception. "Not implemented."))
+  (->> (re-seq #"\w+" x)
+       (map string/lower-case)
+       frequencies)
 )
-
-
-
-
